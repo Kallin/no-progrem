073c7ae45f32998d6e936d189538a15fe9c61811
knagelberg <knagelberg@salesforce.com>
2013-05-30 00:32:31 -0700

    problem 14 finished

---
+++ src/main/groovy/com/noproglem/Problem14.groovy
@@ -0,0 +1,43 @@
+package com.noproglem
+
+class Problem14 extends Problem {
+
+    def solution1() {
+        def maxChain = 0
+        def collatzForMax = 0
+
+        for (long test in 1..given.max) {
+
+            def chainLength = findCollatzLength(test)
+
+            if (chainLength > maxChain) {
+                maxChain = chainLength
+                collatzForMax = test
+            }
+        }
+
+        return collatzForMax
+    }
+
+    def collatzMap = [:]
+    def findCollatzLength(long test) {
+        if (collatzMap[test])
+            return collatzMap[test]
+
+        if (test == 1)
+            return 1
+
+        def collatzValue
+        if (test % 2 == 0) {
+            collatzValue = findCollatzLength((long) test / 2) + 1
+        } else {
+            collatzValue = findCollatzLength(test * 3 + 1) + 1
+        }
+
+        collatzMap[test] = collatzValue
+        return collatzMap[test]
+    }
+
+}
+
+
