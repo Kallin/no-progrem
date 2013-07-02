2216895a1a041767d894b83d270aaaf7694c1a0a
knagelberg <knagelberg@salesforce.com>
2013-07-01 19:13:04 -0700

    restructuring for multi-project build

--- groovy/src/main/groovy/com/noproglem/Problem.groovy
+++ groovy/src/main/groovy/com/noproglem/Problem.groovy
@@ -4,18 +4,17 @@ import groovy.json.JsonSlurper
 
 abstract class Problem {
 
-    def description
     def answer
     def given
 
     Problem() {
         final simpleName = this.class.simpleName.toLowerCase()
 
-        final file = new File("src/main/resources/${simpleName}.json")
+        final file = new File("problems/src/main/resources/${simpleName}.json")
         final slurper = new JsonSlurper()
         def object = slurper.parseText(file.text)
-        ['description','answer','given'].each{
-            this[it]=object[it]
+        ['description', 'answer', 'given'].each {
+            this[it] = object[it]
         }
     }
 
